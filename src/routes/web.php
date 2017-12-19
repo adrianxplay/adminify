325da70fdcd9fd4af6f0165bcce2af082166d682
Adrian Piña <adrianxplay@gmail.com>
2017-12-18 18:01:21 -0600

    fix routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,8 +5,7 @@ Route::prefix('admin')->group(function(){
   $namespace = 'Adrianxplay\Adminify\Http\Controllers\\';
 
   Route::get('dashboard', $namespace.'DashboardController@index');
-
-  Route::get('dashboard/user', $namespace.'DashboardController@list_user');
   Route::get('dashboard/{slug}', $namespace.'DashboardController@list_model');
+  Route::get('dashboard/{slug}/{id}', $namespace.'DashboardController@edit_model');
 
 });
